5c4027a767940780676d8a4235c42699916ba2c7
Trangar <victor.koenders@gmail.com>
2021-06-20 10:58:44 +0200

    Added String tests to validate_struct

--- tests/validate_struct.rs
+++ tests/validate_struct.rs
@@ -60,3 +60,66 @@ fn test_deserialize_upgrade_version() {
     assert_eq!(deserialized.id, 1);
     assert_eq!(deserialized.name, "");
 }
+
+#[cfg(feature = "std")]
+#[derive(Serializable)]
+pub struct TestWithString {
+    #[since(0.0.1)]
+    pub id: u32,
+    #[since(0.0.2)]
+    pub name: String,
+}
+#[cfg(feature = "std")]
+#[test]
+fn test_std_serialize_simple() {
+    // serialize a v2.0.0 struct
+    let test = TestWithString {
+        id: 5,
+        name: "Trangar".to_owned(),
+    };
+    let serialized = binver::to_vec(&test);
+    assert_eq!(
+        vec![
+            0, 0, 0, 5, // id
+            0, 0, 0, 7, b'T', b'r', b'a', b'n', b'g', b'a', b'r', // name
+        ],
+        &serialized[6..] // Ignore version bytes
+    );
+}
+
+#[cfg(feature = "std")]
+#[test]
+fn test_std_simple_deserialize() {
+    // Deserialize a v2.0.0 struct
+    let mut vec = Vec::new();
+    vec.extend_from_slice(&(0u16.to_be_bytes())); // semver major
+    vec.extend_from_slice(&(0u16.to_be_bytes())); // semver minor
+    vec.extend_from_slice(&(2u16.to_be_bytes())); // semver patch
+
+    vec.extend_from_slice(&(1u32.to_be_bytes())); // ID
+
+    vec.extend_from_slice(&(7u32.to_be_bytes())); // name length
+    vec.extend_from_slice(b"Trangar"); // name
+
+    let deserialized: TestWithString = binver::deserialize_slice(&vec).unwrap();
+
+    assert_eq!(deserialized.id, 1);
+    assert_eq!(deserialized.name, "Trangar".to_owned());
+}
+
+#[test]
+#[cfg(feature = "std")]
+fn test_std_deserialize_upgrade_version() {
+    // Deserialize a v1.0.0 struct into v2.0.0
+    let mut vec = Vec::new();
+    vec.extend_from_slice(&(0u16.to_be_bytes())); // semver major
+    vec.extend_from_slice(&(0u16.to_be_bytes())); // semver minor
+    vec.extend_from_slice(&(1u16.to_be_bytes())); // semver patch
+
+    vec.extend_from_slice(&(1u32.to_be_bytes())); // ID
+
+    let deserialized: TestWithString = binver::deserialize_slice(&vec).unwrap();
+
+    assert_eq!(deserialized.id, 1);
+    assert!(deserialized.name.is_empty());
+}
